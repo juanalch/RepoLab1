44477602d9a19234e6e4ce7b4fccdec983dbe98e
LauraVenegas6 <aleja09venegas@gmail.com>
2025-09-03 16:10:40 -0500

    reto 6 metodo ejecutarcomando primeros 4 comando

--- Retos/reto6/MaquinaDecisiones.java
+++ Retos/reto6/MaquinaDecisiones.java
@@ -2,6 +2,15 @@ package Retos.reto6;
 
 public class MaquinaDecisiones {
 
+public void ejecutarComando(String comando) {
+    switch (comando) {
+        case "SALUDAR" -> System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!");
+        case "DESPEDIR" -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión.");
+        case "CANTAR" -> System.out.println("La máquina canta: 01010101");
+        case "DANZAR" -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta.");
+        default -> System.out.println("Comando no reconocido: " + comando);
+    }
+}
 
 
 }
