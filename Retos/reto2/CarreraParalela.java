ef83f81cb14fdd79e7c336e58adb44342e3b68af
LauraVenegas6 <aleja09venegas@gmail.com>
2025-09-03 14:01:39 -0500

    preparacion rero 2
    crea la clase base y hace el primer commit en la rama feature.

--- Retos/reto2/CarreraParalela.java
+++ Retos/reto2/CarreraParalela.java
@@ -1,5 +1,19 @@
 package Retos.reto2;
 
+import java.util.*;
+import java.util.function.Function;
+
 public class CarreraParalela {
-    
+
+    public static void main(String[] args) {
+        // ====== Punto de partida ======
+       
+
+        List<Integer> lista1 = Arrays.asList(10, 20, 5, 7, 30);
+        List<Integer> lista2 = Arrays.asList(3, 9, 12, 1, 25, 40);
+
+    }
+
+
+
 }
