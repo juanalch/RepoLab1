a84bf5e6e7de5e52fbd174ca81158072148ecac6
juanalch <lozanochavesjuana@gmail.com>
2025-09-03 16:10:40 -0500

    Reto6 método ejecutar comando últimos 4 comandos
    Se desarrollo la parte del estudiante b

--- Retos/reto6/MaquinaDecisiones.java
+++ Retos/reto6/MaquinaDecisiones.java
@@ -1,7 +1,15 @@
 package Retos.reto6;
 
 public class MaquinaDecisiones {
-
+    public void ejecutarComando(String comando) {
+        switch (comando) {
+            case "BROMEAR" -> System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio...");
+            case "GRITAR" -> System.out.println("La máquina grita: ¡¡¡ALERTA DE STACK OVERFLOW!!!");
+            case "SUSURRAR" -> System.out.println("La máquina susurra: Shhh... los bugs están dormidos.");
+            case "ANALIZAR" -> System.out.println("La máquina procesa: Analizando datos... resultado: ¡Eres increíble programando!");
+            default -> System.out.println("Comando no reconocido: " + comando);
+        }
+    }
 
 
 }
