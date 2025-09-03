d54610bdaa9e2498225e314b14532b0a790ae092
juanalch <lozanochavesjuana@gmail.com>
2025-09-03 15:33:02 -0500

    Reto 5 estudiante A choque

--- Retos/reto5/BatallaDeConjuntos.java
+++ Retos/reto5/BatallaDeConjuntos.java
@@ -12,5 +12,33 @@ public class BatallaDeConjuntos {
                 .collect(Collectors.toSet()); // HashSet por defecto
     }
 
-    
+    // Estudiante B
+    public static Set<Integer> treeSetSinMultiplosDe5(Set<Integer> numeros) {
+        return numeros.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new)); // mantiene orden natural
+    }
+
+    // Choque
+    public static Set<Integer> unirYOrdenar(Set<Integer> conj1, Set<Integer> conj2) {
+        return Stream.concat(conj1.stream(), conj2.stream())
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de números aleatorios para cada conjunto
+        Set<Integer> conjuntoHash = new HashSet<>(Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5));
+        Set<Integer> conjuntoTree = new TreeSet<>(Arrays.asList(12, 3, 25, 10, 7, 30, 18, 4));
+
+        // Aplicar filtros respectivos
+        Set<Integer> filtradoHash = hashSetSinMultiplosDe3(conjuntoHash);
+        Set<Integer> filtradoTree = treeSetSinMultiplosDe5(conjuntoTree);
+
+        // Unir y ordenar los conjuntos filtrados
+        Set<Integer> union = unirYOrdenar(filtradoHash, filtradoTree);
+
+        // Imprimir resultados con lambda
+        union.forEach(n -> System.out.println("Número en arena: " + n));
+    }
+
 }
