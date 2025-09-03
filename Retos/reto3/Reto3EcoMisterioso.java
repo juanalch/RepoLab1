96bd61137543bbc4d8ce12d368ffd42ea95bc779
LauraVenegas6 <aleja09venegas@gmail.com>
2025-09-03 12:59:53 -0500

    reto3 venegas funcion que combina

--- Retos/reto3/Reto3EcoMisterioso.java
+++ Retos/reto3/Reto3EcoMisterioso.java
@@ -2,6 +2,7 @@ package Retos.reto3;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.function.Function; 
 
 public class Reto3EcoMisterioso {
 
@@ -20,11 +21,26 @@ public class Reto3EcoMisterioso {
         StringBuffer sb = new StringBuffer(mensaje);
         return sb.reverse().toString();
     }
-    
+
+        // Función combinada: repetir 3 veces y luego invertir el resultado
+    public static String repetirYInvertir(String mensaje) {
+        // Repetir 3 veces con stream (como hizo A)
+        String repetido = Stream.generate(() -> mensaje)
+                                .limit(3)
+                                .collect(Collectors.joining(" "));
+        // Invertir con StringBuffer (como hizo B)
+        StringBuffer sb = new StringBuffer(repetido);
+        return sb.reverse().toString();
+    }
 
     public static void main(String[] args) {
+        // Lambda para invocar la función combinada
+        Function<String, String> ecoMisterioso = Reto3EcoMisterioso::repetirYInvertir;
+
         String mensaje = "Hola cueva";
+
         System.out.println("Repetir con StringBuilder: " + repetirConStringBuilder(mensaje));
-        
+        System.out.println("Invertir con StringBuffer: " + invertirConStringBuffer(mensaje));
+        System.out.println("Repetir y luego invertir combinado: " + ecoMisterioso.apply(mensaje));
     }
 }
