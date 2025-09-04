ba5ab93eeb69efd3f6641e1a77dd9572b70f2c85
LauraVenegas6 <aleja09venegas@gmail.com>
2025-09-04 16:32:41 -0500

    reto 4 parte de unificar y hacer merge

--- Retos/reto4/Reto4Tesoro.java
+++ Retos/reto4/Reto4Tesoro.java
@@ -5,13 +5,23 @@ import java.util.stream.Collectors;
 
 
 public class Reto4Tesoro {
-    
-    public static Map<String, Integer> createHashMap(List<Map.Entry<String, Integer>> entries) {
+
+    public static Map<String, Integer> createMap(List<Map.Entry<String, Integer>> entries) {
         Map<String, Integer> map = new HashMap<>();
         for (Map.Entry<String, Integer> entry : entries) {
             map.putIfAbsent(entry.getKey(), entry.getValue());
         }
         return map;
     } 
+
+    
+    public static Map<String, Integer> mergeMaps(
+            Map<String, Integer> hashMap,
+            Map<String, Integer> hashTable) {
+        Map<String, Integer> merged = new HashMap<>(hashMap);
+        merged.putAll(hashTable); // si hay conflicto, Hashtable sobrescribe
+        return merged;
+    }
+
     
 }
